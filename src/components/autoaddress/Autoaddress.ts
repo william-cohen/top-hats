a3970fa21758ddbfe04533f4007684e38b168e98
Will Cohen <will@qkly.com.au>
2021-09-12 23:04:49 +1000

    Updated address autocomplete

--- src/components/autoaddress/Autoaddress.ts
+++ src/components/autoaddress/Autoaddress.ts
@@ -1,9 +1,23 @@
 import { mande } from 'mande'
 import { ref, Ref, watch } from 'vue'
 
-const autoaddress = mande('https://api.addressfinder.io/api/au/address', {
+function debounce<F extends (...args: any) => void, P extends Parameters<F>>(
+  fn: F,
+  delay = 200
+) {
+  let timeout: number | null = null
+  return function (
+    this: (...args: unknown[]) => unknown,
+    ...args: Parameters<F>
+  ) {
+    if (timeout) clearTimeout(timeout)
+    timeout = window.setTimeout(() => fn.apply(this, args), delay)
+  } as F
+}
+
+const autoaddress = mande('https://api.geoapify.com/v1/geocode/autocomplete', {
   headers: {
-    origin: 'https://tophats.website',
+    origin: 'https://tophats.website/',
     referer: 'https://tophats.website/'
   }
 })
@@ -13,12 +27,11 @@ export interface AddressCompletion {
 }
 
 interface CompletionResponse {
-  completions: AddressCompletion[]
-}
-
-interface ValidationResonse {
-  success: boolean
-  matched: boolean
+  features: {
+    properties: {
+      formatted: string
+    }
+  }[]
 }
 
 export const useAddressCompletion = (
@@ -26,20 +39,24 @@ export const useAddressCompletion = (
 ): { completions: Readonly<Ref<AddressCompletion[]>> } => {
   const completions = ref<AddressCompletion[]>([])
 
-  watch(addressRef, (addr) => {
-    autoaddress
-      .get<CompletionResponse>('autocomplete', {
-        query: {
-          q: addr,
-          key: 'JBXLDKVA96YM74FERQCT',
-          format: 'json',
-          max: 7,
-          wv: '3.25.0',
-          session: 'd819b650-85fc-45d4-8ea7-83632bb36722'
-        }
-      })
-      .then((response) => (completions.value = response.completions))
-  })
+  watch(
+    addressRef,
+    debounce((addr) => {
+      autoaddress
+        .get<CompletionResponse>('autocomplete', {
+          query: {
+            text: addr,
+            apiKey: '91ddc8e67d29406b8852c0195ad59dbf'
+          }
+        })
+        .then(
+          (response) =>
+            (completions.value = response.features.map((feature) => ({
+              full_address: feature.properties.formatted
+            })))
+        )
+    }, 300)
+  )
 
   return {
     completions: completions as Readonly<Ref<AddressCompletion[]>>
@@ -51,20 +68,21 @@ export const useAddressValidation = (
 ): { isValid: Readonly<Ref<boolean>> } => {
   const isValid = ref<boolean>(false)
 
-  watch(addressRef, (addr) => {
-    autoaddress
-      .get<ValidationResonse>('verification', {
-        query: {
-          q: addr,
-          key: 'JBXLDKVA96YM74FERQCT',
-          format: 'json',
-          max: 7,
-          wv: '3.25.0',
-          session: '22ab420b-bf58-4fdd-8383-7e57cdb9c26d'
-        }
-      })
-      .then((response) => (isValid.value = response.matched))
-  })
+  watch(
+    addressRef,
+    debounce((addr) => {
+      autoaddress
+        .get<CompletionResponse>('autocomplete', {
+          query: {
+            text: addr,
+            apiKey: '91ddc8e67d29406b8852c0195ad59dbf'
+          }
+        })
+        .then((response) => {
+          isValid.value = response.features.length < 3
+        })
+    }, 300)
+  )
 
   return {
     isValid: isValid as Readonly<Ref<boolean>>
